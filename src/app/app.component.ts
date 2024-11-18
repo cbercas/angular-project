59c469109497db818e817f17e6dc8e2da763ed97
cbercas <cbercas@iescarrillo.es>
2024-11-18 14:14:33 +0100

    video 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RouterLink } from '@angular/router';
+import { TasklistComponent } from './components/task/tasklist/tasklist.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,TasklistComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'angular-project';
+  nombre = 'Cristina'
+
+
+
 }
